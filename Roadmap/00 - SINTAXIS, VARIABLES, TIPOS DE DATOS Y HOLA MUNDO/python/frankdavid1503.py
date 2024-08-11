86f316226dc6be02b27d536f5c3b1c60d2260d53
FrankDavid1503 <168614790+FrankDavid1503@users.noreply.github.com>
2024-08-10 19:39:45 -0500

    Create frankdavid1503.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/frankdavid1503.py
@@ -0,0 +1,18 @@
+# https://python.org
+
+# texto en una linea
+
+"texto en muchas lineas" 
+'texto igualmente en muchas lineas' 
+
+my_variable = "un perro" 
+
+MY_CONSTANT = "una flor" 
+
+my_int = 1 
+my_bool = True 
+my_bool = False 
+my_string = "mi cadena de texto" 
+my_float = 1.5 
+
+print("¡Hola, Phyton!")    
